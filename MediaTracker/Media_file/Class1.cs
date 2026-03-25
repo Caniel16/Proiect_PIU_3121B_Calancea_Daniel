7d76640525cd201d659d2abf47f73c870e1aebad
Caniel16 <121947039+Caniel16@users.noreply.github.com>
2026-03-25 12:07:03 +0200

    Add files via upload

---
+++ MediaTracker/Media_file/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace Media_file
+{
+    public class MediaFile
+    {
+        public string Title;
+        public string Path;
+        public double SavedPosition;
+    }
+    [Flags]
+    public enum Stare
+    {
+        None = 0,
+        Ascultat = 1,
+        Favorit = 2,
+        DeAscultat = 4,
+        Arhivat = 8
+    }
+    public enum Gen
+    {
+        Fantezie,
+        ScientificFiction,
+        Comedie,
+        Thriller,
+        NonFictiune
+    }
+}
---
+++ MediaTracker/Registru_media/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.InteropServices;
+using Media_file;
+namespace Registru_media
+{
+    public class Lista_file
+    {
+        List<MediaFile> files = new List<MediaFile>();
+        
+        public void Add(MediaFile file) {
+            files.Add(file);
+        }
+        public List<MediaFile> GetAll()
+        {
+            return files;
+        }
+        public void load()
+        {
+            Console.WriteLine("Enter the path to your MP3 file:");
+            string path = Console.ReadLine();
+
+            Console.WriteLine("Enter a title for this file:");
+            string title = Console.ReadLine();
+            MediaFile file = new MediaFile();
+            file.Title = title;
+            file.Path = path;
+            file.SavedPosition = 0;
+            files.Add(file);
+            Console.WriteLine("Added: " + title);
+        }
+        public MediaFile Search(string title)
+        {
+            foreach (MediaFile file in files)
+            {
+                if (file.Title == title)
+                    return file;
+            }
+            return null;
+        }
+        public void Afisare()
+        {
+            Console.WriteLine("Your library:");
+            foreach (MediaFile file in files)
+            {
+                Console.WriteLine("- " + file.Title + " (" + file.SavedPosition + "s)");
+            }
+        }
+
+
+    }
+}
